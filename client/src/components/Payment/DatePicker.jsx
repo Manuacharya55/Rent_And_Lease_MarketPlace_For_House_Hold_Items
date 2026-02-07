9f5acc95d7b1e28020a376c7da7a455bbfd494c6
Manu <belveacharya55@gmail.com>
2026-02-07 11:50:17 +0530

    Routes Altered

--- client/src/components/Payment/DatePicker.jsx
+++ client/src/components/Payment/DatePicker.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
-
 const DatePicker = ({ setSelectedDate, selectedDate, bookedDates = [] }) => {
 
   const handleDateClick = (date) => {
