a2f084a1ae794d572cfc6f8ea04465376c8d0812
Manu <101309561+Manuacharya55@users.noreply.github.com>
2026-02-07 11:34:53 +0530

    Update DatePicker.jsx
    Removed unused CSS import for MyCalendar.

--- client/src/components/Payment/DatePicker.jsx
+++ client/src/components/Payment/DatePicker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import "../MyCalendar.css";
+
 
 const DatePicker = ({ setSelectedDate, selectedDate, bookedDates = [] }) => {
 
