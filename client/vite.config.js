e7ff78c042c56ede6d5347425c944c96e78b04b7
Manu <belveacharya55@gmail.com>
2026-02-07 11:05:52 +0530

    project completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:import.meta.env.BASE_URL || "/Rent_And_Lease_MarketPlace_For_House_Hold_Items/tree/main/client"
 })
